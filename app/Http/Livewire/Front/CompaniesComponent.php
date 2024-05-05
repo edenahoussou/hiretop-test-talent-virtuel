2efc3b3944706e486130e9ad82ef5c05b70520fa
Eden <aedenrosario@gmail.com>
2024-05-05 03:23:23 +0100

    [Feat]: Navigation livewire component

---
+++ app/Http/Livewire/Front/CompaniesComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Front;
+
+use Livewire\Component;
+
+class CompaniesComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.companies-component');
+    }
+}
